b77a891bdc6b0b0ddc6a9750d50aa8aaffd0e975
hisokabubble <s10222087@connect.np.edu.sg>
2024-01-06 00:59:19 +0800

    Imported Unlinked Firebase

--- Assg2_MadeInSG_Unity_Project/Assets/Scripts/MyDatabase.cs
+++ Assg2_MadeInSG_Unity_Project/Assets/Scripts/MyDatabase.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Quit : MonoBehaviour
+public class MyDatabase : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,9 +14,5 @@ public class Quit : MonoBehaviour
     void Update()
     {
         
-    }
-    public void QuitGame()
-    {
-        Application.Quit();
     }
 }
